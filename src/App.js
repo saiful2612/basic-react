21bc99dbdb8c7e76d22fa2beb302e1708a049009
saiful2612 <bristykodom@gmail.com>
2020-10-01 23:46:06 +0600

    React basic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let person={
+    name:'Riyaz',
+    job:'Full stack Web Developer'
+  }
+
+  const products =[
+    {name:"Photohop",price:"$90.99"},
+    {name:"Illustrator",price:"$70.25"},
+    {name:"PDF Reader",price:"$11.99"},
+    {name: "Adobe", price:"$49.99"},
+    {name:"Foxit Reader",price:"$111.99"}
+  ]
+
+  const productNames=products.map(product => product.price)
+  console.log(productNames)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am a react developer</p>
+
+        {
+          products.map(product => <Product product={product}></Product>)
+        }
+
+        <Counter></Counter>
+        <Crickter name="Akram Khan" job="batsman"></Crickter>
+        <Crickter name="Pailot" job="Wicket Keeper"></Crickter>
+        <Crickter name="Rafique" job="Bowler" ></Crickter>
       </header>
     </div>
   );
+}
+
+function Counter(){
+  const[count,setCount]=useState(10);
+  const handleDecrease= () => setCount(count -1);
+  const handleIncrease= () => setCount(count +1);
+  return(
+    <div>
+      <h3>Count:{count}</h3>
+      <button onClick={handleDecrease}>Decrease</button>
+      <button onClick={handleIncrease}>Increase</button>
+    </div>
+  )
+}
+
+function Product(props){
+  const productStyle={
+    border:"1px solid gray",
+    borderRadius:"5px",
+    backgroundColor:"lightgray",
+    height:"200px",
+    width:"200px",
+    float:"left"
+  }
+  return(
+    <div style={productStyle}>
+      <h3>Name:{props.product.name }</h3>
+      <h4>Price {props.product.price}</h4>
+      <button>Buy now</button>
+    </div>
+  )
+}
+
+function Crickter(props){
+  return (
+  <div style={{border:'1px solid red',margin:'10px',padding:'20px'}}>
+      <h1>Name: {props.name}</h1>
+      <h3>Bangladeshi Crickter</h3>
+      <p>Playing Role: {props.job}</p>
+    </div>
+
+  )
 }
 
 export default App;
